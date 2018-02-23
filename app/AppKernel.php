8ad4398f0580ec096b3150b94be7545796c4d703
elchris <frenchy@gmail.com>
2018-02-22 22:35:47 -0600

    - created custom route yml file for "test" environment, referenced from config_test.yml. This was to avoid referencing OAuthServer routes which made the controller test blow-up, which I hadn't previously caught due to caching. - upgraded symfony to 3.4.4

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
-            new AppBundle\AppBundle(),
+            new AppBundle\AppBundle()
         ];
 
         if (! in_array($this->getEnvironment(), ['test'], true)) {
