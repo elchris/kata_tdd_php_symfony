5a4cc4d012a8277432a8aa738948c00d02960c17
elchris <frenchy@gmail.com>
2019-05-25 18:37:10 -0500

    laid-down the failing acceptance tests for the first module

--- tests/api/RegisterUserCest.php
+++ tests/api/RegisterUserCest.php
@@ -10,5 +10,58 @@ class RegisterUserCest
      */
     public function seeNewUserCreated(ApiTester $I)
     {
+        $response = $I->sendPostApiRequest(
+            '/register-user',
+            [
+                'firstName' => 'chris',
+                'lastName' => 'holland'
+            ]
+        );
+
+        $I->canSeeResponseContainsJson(
+            [
+                'firstName' => 'chris',
+                'lastName' => 'holland'
+            ]
+        );
+
+        $userId = $response['id'];
+
+        $I->sendPatchApiRequest(
+            '/user/'.$userId,
+            [
+                'role' => 'Passenger'//AppRole::PASSENGER_NAME
+            ]
+        );
+        $I->canSeeResponseContainsJson(
+            [
+                'roles' => [
+                    0 => [
+                        'id' => 1,
+                        'name' => 'Passenger'//AppRole::PASSENGER_NAME
+                    ]
+                ]
+            ]
+        );
+        $I->sendPatchApiRequest(
+            '/user/'.$userId,
+            [
+                'role' => 'Driver'//AppRole::DRIVER_NAME
+            ]
+        );
+        $I->canSeeResponseContainsJson(
+            [
+                'roles' => [
+                    0 => [
+                        'id' => 1,
+                        'name' => 'Passenger'//AppRole::PASSENGER_NAME
+                    ],
+                    1 => [
+                        'id' => 2,
+                        'name' => 'Driver'//AppRole::DRIVER_NAME
+                    ]
+                ]
+            ]
+        );
     }
 }
