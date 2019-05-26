bf68e228e6b1855406612e234e7cbf5b2c5cbe12
elchris <frenchy@gmail.com>
2019-05-26 12:25:53 -0500

    laid-down the acceptance test for assigning a destination to a ride

--- tests/api/CreateRideCest.php
+++ tests/api/CreateRideCest.php
@@ -44,5 +44,25 @@ class CreateRideCest
                 'driver_id' => $createdDriverId
             ]
         );
+
+        $I->sendPatchApiRequest(
+            '/ride/'.$createdRideId,
+            [
+                'destinationLat' => LocationRepositoryTest::WORK_LOCATION_LAT,
+                'destinationLong' => LocationRepositoryTest::WORK_LOCATION_LONG
+            ]
+        );
+
+        $I->seeResponseContainsJson(
+            [
+                'id' => $createdRideId,
+                'passenger_id' => $createdPassengerId,
+                'departure_lat' => LocationRepositoryTest::HOME_LOCATION_LAT,
+                'departure_long' => LocationRepositoryTest::HOME_LOCATION_LONG,
+                'driver_id' => $createdDriverId,
+                'destination_lat' => LocationRepositoryTest::WORK_LOCATION_LAT,
+                'destination_long' => LocationRepositoryTest::WORK_LOCATION_LONG
+            ]
+        );
     }
 }
